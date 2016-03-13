1a69505402cca9fbe0dfc536ac4b54dba2069176
Hum4n01d <krwillx@icloud.com>
2016-03-13 16:45:13 -0700

    Made basic Jade files and Stylus. Also http.cat errors

--- app.js
+++ app.js
@@ -36,15 +36,15 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+//if (app.get('env') === 'development') {
+//  app.use(function(err, req, res, next) {
+//    res.status(err.status || 500);
+//    res.render('error', {
+//      message: err.message,
+//      error: err
+//    });
+//  });
+//}
 
 // production error handler
 // no stacktraces leaked to user
@@ -52,6 +52,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
+    code: err.status,
     error: {}
   });
 });
