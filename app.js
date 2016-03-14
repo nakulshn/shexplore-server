078b0ed20535b1d6222a5ace099d0d9a117759d5
Hum4n01d <krwillx@icloud.com>
2016-03-13 19:43:13 -0700

    Added dependencies and defined app

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var knex = require('knex')({
 var Bookshelf = require('bookshelf');
 
 var bookshelf = Bookshelf(knex);
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
