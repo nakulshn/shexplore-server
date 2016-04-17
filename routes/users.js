a353e2635096a85b4cc9f13387a7dbf648dff3db
pfgithub <omegariisthebest@gmail.com>
2016-04-17 14:59:15 -0700

    Added profile

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/profile', function(req, res, next) {
+  res.render("profile",{username:"unknown",level:1,badges:[],projects:[]});
+});
+
 module.exports = router;
